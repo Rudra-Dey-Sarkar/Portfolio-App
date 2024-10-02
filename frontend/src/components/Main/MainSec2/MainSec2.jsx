26f404568041d9388e65a3be9d3e523ef7ceea7c
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-10-02 15:27:39 +0530

    Update MainSec2.jsx

--- frontend/src/components/Main/MainSec2/MainSec2.jsx
+++ frontend/src/components/Main/MainSec2/MainSec2.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 import "./MainSec2.css"
 import axios from "axios"
 import { SoftwareDeveloper, Gamer, Athlete  } from '../../Icons/Icons';
+import { motion } from "framer-motion"
 function MainSec2() {
-  const [datas, setDatas] = useState([]);
+  const [datas, setDatas] = useState();
 
   useEffect(() => {
     try {
-      axios.get("https://portfolio-app-backend-five.vercel.app/get-main-datas").then((response) => {
-        setDatas(response.data);
+      axios.get("http://localhost:5000/get-main-datas").then((response) => {
+        setTimeout(()=>{
+          setDatas(response.data);
+        }, 1000);
 
       }).catch((err) => {
         console.log("data not fetched due to :-", err);
@@ -20,17 +23,37 @@ function MainSec2() {
 
   return(
     <div>
-      {datas.map((e, index)=>(
-      <section key={index} className='main-sec2'>
-        <div className='about-texts'>
-          <div className='intro-name'><p>I'm</p><p>Rudra</p></div>
-          <div className='software-developer'><p>a Software<SoftwareDeveloper/>Developer</p></div>
-          <p className='gamer'> a Passionate <Gamer/> Gamer</p>
-          <p className='gamer'> a Passionate <Athlete/> Athlete</p>
+            {!datas ? (
+                <div style={{backgroundColor:"#081b29" ,width: "100%", height: "100vh", display:"flex", justifyContent:"center", alignItems:"center" }}>
+                <motion.div
+                  style={{ backgroundColor: "#0ef", width: "100px", height: "100px" }}
+                  animate={{
+                    scale: [1, 2, 2, 1, 1],
+                    rotate: [0, 0, 270, 270, 0],
+                    borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+                  }}
+                  transition={{
+                    duration: 2,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }} />
+              </div>
+      ) : (
+        <div>
+          {datas.map((e, index)=>(
+          <section key={index} className='main-sec2'>
+            <div className='about-texts'>
+              <div className='intro-name'><p>I'm</p><p>Rudra</p></div>
+              <div className='software-developer'><p>a Software<SoftwareDeveloper/>Developer</p></div>
+              <p className='gamer'> a Passionate <Gamer/> Gamer</p>
+              <p className='gamer'> a Passionate <Athlete/> Athlete</p>
+            </div>
+            <img src={e.sec2.pics.pic1}></img>
+          </section>
+          ))}
         </div>
-        <img src={e.sec2.pics.pic1}></img>
-      </section>
-      ))}
+      )}
+      
     </div>
   )
 }
