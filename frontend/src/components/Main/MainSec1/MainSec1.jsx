115768c8123608002a7ec26f00a72e8521c16ebe
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-10-02 16:09:23 +0530

    Update MainSec1.jsx

--- frontend/src/components/Main/MainSec1/MainSec1.jsx
+++ frontend/src/components/Main/MainSec1/MainSec1.jsx
@@ -10,10 +10,7 @@ function MainSec1() {
   useEffect(() => {
     try {
       axios.get("http://localhost:5000/get-main-datas").then((response) => {
-        setTimeout(() => {
-          setDatas(response.data);
-        }, 1000);
-
+setDatas(response.data);
       }).catch((err) => {
         console.log("data not fetched due to :-", err);
       })
