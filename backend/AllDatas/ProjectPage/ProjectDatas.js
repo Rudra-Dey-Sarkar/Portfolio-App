e9ba1dd20453c54144de08a1a044473437767831
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-10-06 14:56:06 +0530

    Update ProjectDatas.js

--- backend/AllDatas/ProjectPage/ProjectDatas.js
+++ backend/AllDatas/ProjectPage/ProjectDatas.js
@@ -9,7 +9,7 @@ module.exports.ProjectDatas = [
                         pic1: "https://raw.githubusercontent.com/Rudra-Dey-Sarkar/Portfolio-App/refs/heads/main/Images/portfolio.png",
                     },
                     link: "https://portfolio-app-frontend-delta.vercel.app/",
-                    p_details: "test"
+                    p_details: "This is my portfolio where you can know more about me, the works I did, the projects that I made, see my skills, download my CV and contact me etc. "
                 },
                 {
                     p_name: "Rock Paper Scissors Game Website",
@@ -17,7 +17,7 @@ module.exports.ProjectDatas = [
                         pic1: "https://raw.githubusercontent.com/Rudra-Dey-Sarkar/Portfolio-App/refs/heads/main/Images/rock-paper-scissors.png",
                     },
                     link: "https://rudra-dey-sarkar.github.io/Rock-Paper-Scissor-Game/",
-                    p_details: "test"
+                    p_details: "This is a Rock Paper Scissors game which is one of the part of my game projects that I build so far, in this game you play with a computer, you can also select the rounds you wanna play for with the computer."
                 },
                 {
                     p_name: "College ERP System Website",
@@ -25,7 +25,7 @@ module.exports.ProjectDatas = [
                         pic1: "https://raw.githubusercontent.com/Rudra-Dey-Sarkar/Portfolio-App/refs/heads/main/Images/college-erp-system.png",
                     },
                     link: "https://college-erp-system-updated-frontend.vercel.app/",
-                    p_details: "test"
+                    p_details: "This is a ERP system for a college where user can explore the courses, departments etc. Students can apply for admission,register and login, see and download the new notices and their attendance, edit their details etc. And faculty can register and login and upload new notice and attendance for the students and admin can add, update and delete the users."
                 }
             ]
         }
