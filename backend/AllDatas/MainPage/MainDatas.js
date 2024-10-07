ccab2712f093dc24ffd5e606b71f2122c0ef6351
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-10-07 12:29:16 +0530

    Update MainDatas.js

--- backend/AllDatas/MainPage/MainDatas.js
+++ backend/AllDatas/MainPage/MainDatas.js
@@ -3,11 +3,11 @@ module.exports.MainDatas = [
         sec1: {
 
             main_text1: "I'm Rudra Dey Sarkar",
-            intro_text1: "A passionate and skilled software developer with experience in MERN Stack, HTML5, CSS3, and JavaScript, TypeScript. Proficient in developing dynamic web applications and projects, including College ERP System, portfolio websites, and interactive games. Adept in using frameworks and tools such as Bootstrap, Git, and GitHub, Vercel, also skilled in Java, C, C++.",
+            intro_text1: "A passionate and skilled software developer with experience in Next.js, Strapi, MERN Stack, HTML5, CSS3, and JavaScript, Typescript. Proficient in developing dynamic web applications and projects, including  ERP Systems, portfolio websites, and interactive web games. Adept in using frameworks and tools such as Bootstrap, Git, and GitHub, Vercel, Netlify, Expo, also possess basic skills in Java, C & C++.",
             pics: {
                 pic1: "https://raw.githubusercontent.com/Rudra-Dey-Sarkar/Portfolio-App/refs/heads/main/Images/websitebackground4.png",
             },
-            pdf_link:"https://raw.githubusercontent.com/Rudra-Dey-Sarkar/Portfolio-App/2491eb5670b553c264ab61d970622e470116b09b/Documents/RDS%20New%20Resume.pdf",
+            pdf_link:"https://raw.githubusercontent.com/Rudra-Dey-Sarkar/Portfolio-App/681ede137a7e7fc7f9cff693706cccc42f0cebfa/Document/RDS%20New%20Resume.pdf",
             pdf_name:"RDS_New_Resume.pdf",
 
         },
